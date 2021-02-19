ca44cb3f3b4a1cdbcbbb73e34b5df3205095cb60
이진영 <codingzombie82@gmail.com>
2021-02-19 14:27:55 +0900

    Splash image wait time Add

--- app/src/main/java/com/changzakso/dreamcatcher/SplashActivity.java
+++ app/src/main/java/com/changzakso/dreamcatcher/SplashActivity.java
@@ -2,7 +2,9 @@ package com.changzakso.dreamcatcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                movePermissionPage();
+            }
+        }, 1500);
+    }
+
+    private void movePermissionPage() {
+        Intent intent = new Intent(SplashActivity.this, PermissionActivity.class);
+        startActivity(intent);
+
+        finish();
     }
 }
